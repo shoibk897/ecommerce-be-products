94b1ab808bd0ef14dd55750abda44b8d49c3bec6
shoibk897 <shoibk897@google.com>
2025-11-16 12:33:23 +0530

    Removed MapStruct and added manual mapper

--- src/main/java/com/shoib/ecommerce/mapper/ProductMapper.java
+++ src/main/java/com/shoib/ecommerce/mapper/ProductMapper.java
@@ -3,14 +3,48 @@ package com.shoib.ecommerce.mapper;
 import com.shoib.ecommerce.dto.ProductAdminDTO;
 import com.shoib.ecommerce.dto.ProductUserDTO;
 import com.shoib.ecommerce.entity.Product;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
-public interface ProductMapper {
+public class ProductMapper {
 
-    ProductAdminDTO toAdminDTO(Product product);
-    ProductUserDTO toUserDTO(Product product);
-    Product toEntity(ProductAdminDTO dto);
+    public static ProductAdminDTO toAdminDTO(Product product) {
+        if (product == null) return null;
 
+        ProductAdminDTO dto = new ProductAdminDTO();
+        dto.setId(product.getId());
+        dto.setName(product.getName());
+        dto.setDescription(product.getDescription());
+        dto.setImageURL(product.getImageURL());
+        dto.setPrice(product.getPrice());
+        dto.setType(product.getType());
+        dto.setStock(product.getStock());
+
+        return dto;
+    }
+
+    public static ProductUserDTO toUserDTO(Product product) {
+        if (product == null) return null;
+
+        ProductUserDTO dto = new ProductUserDTO();
+        dto.setId(product.getId());
+        dto.setName(product.getName());
+        dto.setDescription(product.getDescription());
+        dto.setImageURL(product.getImageURL());
+        dto.setPrice(product.getPrice());
+        dto.setType(product.getType());
+
+        return dto;
+    }
+
+    public static Product toEntity(ProductAdminDTO dto) {
+        if (dto == null) return null;
+        Product product = new Product();
+        product.setId(dto.getId());
+        product.setName(dto.getName());
+        product.setDescription(dto.getDescription());
+        product.setImageURL(dto.getImageURL());
+        product.setPrice(dto.getPrice());
+        product.setType(dto.getType());
+        product.setStock(dto.getStock());
+        return product;
+    }
 }
